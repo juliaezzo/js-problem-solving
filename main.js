547c9dd1422933920f3db4ba030197e88cdbfefa
juliaezzo <ezzojulia5@gmail.com>
2025-09-05 14:23:22 +0300

    add exercise 2 & 3

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
 // 1:write a function to calculate the hypotenuse of a right triangle.
-const hypotenuse = (a,b) => {
- let hyp = 0;
- res = (a**2) + (b**2)
- for(let i = 1; i <= res; i++){
-    if(i * i === res) {
-       i = hyp
-    }
- }
- return hyp;
-};
+// const hypotenuse = (a,b) => {
+//  let hyp = 0;
+//  res = (a**2) + (b**2)
+//  for(let i = 1; i <= res; i++){
+//     if(i * i === res) {
+//        i = hyp
+//     }
+//  }
+//  return hyp;
+// };
+
+
+
+
+
+// 2:write a function to calculate the factorial of number for example the factorial of 3 is 3 * 2 *1 = 6
+// طريقة الاولى
+//    const calculateFactorial = (num) => {
+   // if(num < 0) {
+   //    return "undefind";
+   // }
+//    let res = 1;
+//    for(let i = 1; i <= num; i++) {
+//       res = res *i
+//    }
+//    return res;
+//  }
+// الطريقة الثانية
+// const calculateFactorial = (num) => {
+//  let res = 1;
+//  for(let i = 5; i > 0; i--) {
+//    res = res * i
+//     console.log(res);
+//  }
+// }
+
+
+
+
+
+// 3:write a function to calculate a power of a number
+// const calculatePower = (base,power) => {
+//    let base = 0;
+//    let power = 0;
+//    let res = 1;
+//    for(let i = 0; i <= power; i++ ) {
+//       res = res * base
+//    }
+// }
+
+
+
+
